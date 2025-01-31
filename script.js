c53b05c2f67c472736856f5c2628c176d926d03c
Mst. Shahena Akter <sa4389310@gmail.com>
2025-01-31 16:17:51 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// alert("bangladesh");
+
+// const fruits = ["apple", "banana", "cherry"];
+// console.log(fruits);
+
+// fruits[0] = "pineApple"
+// console.log(fruits)
+
+const fruits1 = ["apple", "banana", "cherry"];
+console.log(fruits1);
+fruits1[3] = "blueberry";
+console.log(fruits1.fruits1);
+
